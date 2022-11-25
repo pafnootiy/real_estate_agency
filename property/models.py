8647d2aec1694bbe29d14a815d024c9d5236eb57
pafnootiy <ksander-86@mail.ru>
2022-11-25 06:07:16 +0400

    remove several fiealds from model

--- property/models.py
+++ property/models.py
@@ -7,12 +7,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер телефона',
-                                        region="RU", blank=True)
-
-
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
